6f5674a5b3c7d606ac48cd65881bcf6db199dd13
Avery66 <1064614313@qq.com>
2026-03-08 15:43:46 +0800

    feat: 系统提示添加非用户输入标记

--- opencode-plugin/tools/index.ts
+++ opencode-plugin/tools/index.ts
@@ -46,8 +46,7 @@ function loopUpdateSubSessionStatus({ client }: { client: OpencodeClient }) {
             agent: "synapse",
             parts: [{
               type: "text",
-              text: `系统提示：${sessionId} 任务已完成\n请使用synapse-task-query工具查询任务完成详情`,
-              synthetic: true
+              text: `[来自系统提示(非用户输入)] ${sessionId} 任务已完成\n请使用synapse-task-query工具查询任务完成详情`
             }]
           },
           path: { id: targetSessionInstance.parentSessionId },
