217c3bb19bd0f31b5350efa694f81bc398168814
Avery66 <1064614313@qq.com>
2026-03-12 13:03:52 +0800

    清理未使用的命令和开发依赖

--- opencode-plugin/commands/index.ts
+++ opencode-plugin/commands/index.ts
@@ -1,11 +1,7 @@
-import SynapseSymmary from "./src/synapseSummary"
-import TrainBusiness from "./src/trainBusiness"
 import DeepInitAgentsMd from "./src/deepInitAgentsMd"
 import UpdateAgentsMd from "./src/updateAgentsMd"
 
 export const SynapseCommand = {
-    "synapse-summary": SynapseSymmary,
-    "train-business": TrainBusiness,
     "deep-init-agents-md": DeepInitAgentsMd,
     "update-agents-md": UpdateAgentsMd
 }
